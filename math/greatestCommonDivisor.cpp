1d0a5c1d38c4b67096b5e91fb515a799a65b1ecd
lfmiranda <luisfmir@gmail.com>
2017-09-09 17:15:05 -0300

    Update "greatest common divisor"

--- math/greatestCommonDivisor.cpp
+++ math/greatestCommonDivisor.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/*
+ * - Problem:
+ * Greatest Common Divisor.
+ *
+ * - Problem description:
+ * Given 2 non-negative integers m and n, find gcd(m, n).
+ * Both m and n fit in a 32 bit signed integer.
+ */
+
 int greatestCommonDivisor(int m, int n) {
     if (n > m) swap(m,n);
     if (n == 0) return m;
